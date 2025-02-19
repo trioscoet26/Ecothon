553f6851bb520b83dc0f5342ac5f03ec9e6f30b7
omkarshelke2005 <shelkeom230@gmail.com>
2025-02-19 11:16:42 +0530

    animals model

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template_string
+import pickle
 from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 
+# Load the trained model once when the app starts
+with open('wildlife_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+    
+    
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello from Flask....!'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
